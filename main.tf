77f688484fcbaa7f84c66816df3868124cccb579
prabala <radhakrishnaprabala8@gmail.com>
2024-10-10 23:18:27 +0530

    main.tf vr1

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier = ""
+  engine = ""
+  engine_version = ""
+  master_username = ""
+  master_password = ""
+  backup_retention_period =
+  preferred_backup_window =
+  skip_final_snapshot =
+  db_subnet_group_name =
+  vpc_security_group_ids =
+  port =
+  db_cluster_parameter_group_name =
+  storage_encrypted =
+  kms_key_id =
+
+}
