2aa32b6328116fe031742afd39c128cfbdd9bc83
prabala <radhakrishnaprabala8@gmail.com>
2024-10-11 22:13:59 +0530

    main.tf vr1

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier              = ""
+  cluster_identifier              = "${var.name}-${var.env}"
   engine                          = "docdb"
-  engine_version                  = ""
-  master_username                 = ""
-  master_password                 = ""
-  backup_retention_period         =
-  preferred_backup_window         =
-  skip_final_snapshot             =
+  engine_version                  = var.engine_version
+  master_username                 =
+  master_password                 =
+  backup_retention_period         = 5
+  preferred_backup_window         = "07:00-09:00"
+  skip_final_snapshot             = true
   db_subnet_group_name            =
   vpc_security_group_ids          =
-  port                            =
+  port                            = var.port
   db_cluster_parameter_group_name =
-  storage_encrypted               =
-  kms_key_id                      =
-  tags                            =
+  storage_encrypted               = true
+  kms_key_id                      = var.kms_key_id
+  tags                            = merge(var.tags, {Name="${var.name}-${var.env}-docdb"})
 }
