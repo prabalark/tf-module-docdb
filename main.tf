64322a302fcdd6dba1c92e520eb7ccea02724de8
prabala <radhakrishnaprabala8@gmail.com>
2024-10-11 23:03:37 +0530

    data.tf vr1

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ resource "aws_docdb_cluster" "main" {
   cluster_identifier              = "${var.name}-${var.env}"
   engine                          = "docdb"
   engine_version                  = var.engine_version
-  master_username                 =
-  master_password                 =
+  master_username                 = data.aws_ssm_parameter.db_user.value
+  master_password                 = data.aws_ssm_parameter.
   backup_retention_period         = 5
   preferred_backup_window         = "07:00-09:00"
   skip_final_snapshot             = true
