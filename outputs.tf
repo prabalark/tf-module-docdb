602587aa460cd50908bde98b62167c09c659d98d
prabala <radhakrishnaprabala8@gmail.com>
2024-10-13 11:39:08 +0530

    outputs.tf vr1

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
 output "subnets" {
-  value = "module.subnets"
+  value = module.subnets
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
 output "subnet_ids" {
@@ -9,3 +13,4 @@ output "subnet_ids" {
 output "subnet_cidrs" {
   value = aws_subnet.main.*.cidr_block
 }
+
